90a6b4943154b013040efb2acf25ebc9b68176d0
Lázaro Nixon <lazaronixon@hotmail.com>
2020-04-05 20:43:23 -0300

    20 days for datasource

--- app/models/corona_datum.rb
+++ app/models/corona_datum.rb
@@ -22,7 +22,7 @@ class CoronaDatum < ApplicationRecord
     end
 
     def datasource_for(state, field, label)
-      result = where(state: state).chronologically.last(25)
+      result = where(state: state).chronologically.last(20)
 
       {
         labels: result.pluck(:reported_at),
