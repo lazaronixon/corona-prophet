17138e40dfc0773608844f5b25845918fedbc3eb
Lázaro Nixon <lazaronixon@hotmail.com>
2020-04-06 14:56:15 -0300

    format legend

--- app/javascript/controllers/chart_line_controller.js
+++ app/javascript/controllers/chart_line_controller.js
@@ -3,10 +3,14 @@ import { Chart } from "chart.js"
 
 export default class extends Controller {
   connect() {
-    new Chart(this.element, { type: "line", data: this.datasource, options: { animation: false } })
+    new Chart(this.element, { type: "line", data: this.datasource, options: { animation: false, scales: this.scales } })
   }
 
   get datasource() {
     return JSON.parse(this.data.get("datasource"))
+  }
+
+  get scales() {
+    return { xAxes: [{ type: 'time', time: { displayFormats: { day: 'DD/MM' } } }] }
   }
 }
