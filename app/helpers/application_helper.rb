611d5b62e32fd73193596960fe32ca53177cf39c
Lázaro Nixon <lazaronixon@hotmail.com>
2020-04-06 00:11:43 -0300

    propheted_at

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def page_title
     content_for(:page_title) || Rails.application.class.to_s.split('::').first
   end
+
+  def propheted_at
+    CoronaDatum.minimum(:created_at).to_date
+  end
 end
