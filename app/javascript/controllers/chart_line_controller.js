b92d18705bc13cc87951dfe83140fe17db7d9531
Lázaro Nixon <lazaronixon@hotmail.com>
2020-04-07 01:11:54 -0300

    animation

--- app/javascript/controllers/chart_line_controller.js
+++ app/javascript/controllers/chart_line_controller.js
@@ -3,7 +3,7 @@ import { Chart } from "chart.js"
 
 export default class extends Controller {
   connect() {
-    new Chart(this.element, { type: "line", data: this.datasource, options: { animation: false, scales: this.scales } })
+    new Chart(this.element, { type: "line", data: this.datasource, options: { scales: this.scales } })
   }
 
   get datasource() {
